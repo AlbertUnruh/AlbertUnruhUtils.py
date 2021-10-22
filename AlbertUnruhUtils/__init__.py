bc9523172c172775b769814ecc35572cea56fc4e
AlbertUnruh <keenan.noack@gmail.com>
2021-10-22 12:26:55 +0200

    setup

---
+++ AlbertUnruhUtils/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "AlbertUnruh"
+__url__ = "https://github.com/AlbertUnruh/AlbertUnruhUtils.py"
+__license__ = "MIT"
+__copyright__ = f"(c) {__author__}"
+__version__ = "2021.10.22.000"  # pattern: 'YYYY.MM.DD.{increment:3}'
+__description__ = "A collection of utils written in Python."
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import re
+
+
+with open("AlbertUnruhUtils/__init__.py") as f:
+    file = f.read()
+
+version = re.search(r"^__version__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", file, re.MULTILINE).group(1)
+url = re.search(r"^__url__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", file, re.MULTILINE).group(1)
+license = re.search(r"^__license__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", file, re.MULTILINE).group(1)  # noqa
+author = re.search(r"^__author__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", file, re.MULTILINE).group(1)
+description = re.search(r"^__description__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", file, re.MULTILINE).group(1)
+
+
+with open("requirements.txt") as f:
+    requirements = f.readlines()
+
+
+setup(
+    name="AlbertUnruhUtils",
+    version=version,
+    packages=["AlbertUnruhUtils"],
+    url=url,
+    license=license,
+    author=author,
+    description=description,
+    install_requires=requirements
+)
