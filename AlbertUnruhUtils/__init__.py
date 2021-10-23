cdc2397b3d10fc90c6a47c4cc86e0a0cb8396c36
AlbertUnruh <keenan.noack@gmail.com>
2021-10-23 14:53:09 +0200

    breaking change while importing remove the ``*`` in the ``__init__.py``

--- AlbertUnruhUtils/__init__.py
+++ AlbertUnruhUtils/__init__.py
@@ -2,8 +2,8 @@ __author__ = "AlbertUnruh"
 __url__ = "https://github.com/AlbertUnruh/AlbertUnruhUtils.py"
 __license__ = "MIT"
 __copyright__ = f"(c) {__author__}"
-__version__ = "2021.10.23.000"  # pattern: 'YYYY.MM.DD.{increment:3}'
+__version__ = "2021.10.23.001"  # pattern: 'YYYY.MM.DD.{increment:3}'
 __description__ = "A collection of utils written in Python."
 
-from .config import *
-from .ratelimit import *
+import config
+import ratelimit
