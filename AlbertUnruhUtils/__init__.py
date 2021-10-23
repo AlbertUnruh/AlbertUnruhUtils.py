e92046bd7bce669af4fa63719008c10f69b25851
AlbertUnruh <keenan.noack@gmail.com>
2021-10-23 14:55:56 +0200

    fix version

--- AlbertUnruhUtils/__init__.py
+++ AlbertUnruhUtils/__init__.py
@@ -2,7 +2,7 @@ __author__ = "AlbertUnruh"
 __url__ = "https://github.com/AlbertUnruh/AlbertUnruhUtils.py"
 __license__ = "MIT"
 __copyright__ = f"(c) {__author__}"
-__version__ = "2021.10.23.001"  # pattern: 'YYYY.MM.DD.{increment:3}'
+__version__ = "2021.10.23.002"  # pattern: 'YYYY.MM.DD.{increment:3}'
 __description__ = "A collection of utils written in Python."
 
 import config
