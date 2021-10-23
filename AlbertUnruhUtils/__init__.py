2da0f5ad912b06f5c98ffdf9523de3b43ccad615
AlbertUnruh <keenan.noack@gmail.com>
2021-10-23 15:29:53 +0200

    fix imports

--- AlbertUnruhUtils/__init__.py
+++ AlbertUnruhUtils/__init__.py
@@ -2,8 +2,11 @@ __author__ = "AlbertUnruh"
 __url__ = "https://github.com/AlbertUnruh/AlbertUnruhUtils.py"
 __license__ = "MIT"
 __copyright__ = f"(c) {__author__}"
-__version__ = "2021.10.23.004"  # pattern: 'YYYY.MM.DD.{increment:3}'
+__version__ = "2021.10.23.005"  # pattern: 'YYYY.MM.DD.{increment:3}'
 __description__ = "A collection of utils written in Python."
 
-import config
-import ratelimit
+from . import config
+from . import ratelimit
+
+from .config import *
+from .ratelimit import *
