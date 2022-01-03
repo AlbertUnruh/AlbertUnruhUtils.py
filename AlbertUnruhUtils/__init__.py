b0168acf437e55c2b03ff5fb3b5caab979ca4861
AlbertUnruh <keenan.noack@gmail.com>
2022-01-03 01:20:51 +0100

    fix version 😅😅😅😅 the day is now correct... 😅😅😅😅

--- AlbertUnruhUtils/__init__.py
+++ AlbertUnruhUtils/__init__.py
@@ -2,7 +2,7 @@ __author__ = "AlbertUnruh"
 __url__ = "https://github.com/AlbertUnruh/AlbertUnruhUtils.py"
 __license__ = "MIT"
 __copyright__ = f"(c) {__author__}"
-__version__ = "2022.01.02.001"  # pattern: 'YYYY.MM.DD.{increment:3}'
+__version__ = "2022.01.03.001"  # pattern: 'YYYY.MM.DD.{increment:3}'
 __description__ = "A collection of utils written in Python."
 
 from . import asynchronous
