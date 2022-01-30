6a29c6811ac74f4fc9d2ba95681e0ab02956eb24
AlbertUnruh <keenan.noack@gmail.com>
2022-01-30 12:30:48 +0100

    move async-requirements to extras_require

--- AlbertUnruhUtils/__init__.py
+++ AlbertUnruhUtils/__init__.py
@@ -2,7 +2,7 @@ __author__ = "AlbertUnruh"
 __url__ = "https://github.com/AlbertUnruh/AlbertUnruhUtils.py"
 __license__ = "MIT"
 __copyright__ = f"(c) {__author__}"
-__version__ = "1.0.1"
+__version__ = "1.1.0a"
 __description__ = "A collection of utils written in Python."
 
 from . import asynchronous
