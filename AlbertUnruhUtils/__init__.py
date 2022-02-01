3456db7409c7aa9e9dc767006ffc7027476dd127
AlbertUnruh <keenan.noack@gmail.com>
2022-02-01 10:37:07 +0100

    fix version number (v2.0.1rc to v2.1.0)

--- AlbertUnruhUtils/__init__.py
+++ AlbertUnruhUtils/__init__.py
@@ -2,7 +2,7 @@ __author__ = "AlbertUnruh"
 __url__ = "https://github.com/AlbertUnruh/AlbertUnruhUtils.py"
 __license__ = "MIT"
 __copyright__ = f"(c) {__author__}"
-__version__ = "2.0.1rc"
+__version__ = "2.1.0rc"
 __description__ = "A collection of utils written in Python."
 
 from . import config
