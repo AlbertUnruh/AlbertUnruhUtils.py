4741215577a395ce4ec74996afc51b08df26c9c5
AlbertUnruh <73029826+AlbertUnruh@users.noreply.github.com>
2022-10-25 23:07:28 +0200

    fix version

--- AlbertUnruhUtils/__init__.py
+++ AlbertUnruhUtils/__init__.py
@@ -2,7 +2,7 @@ __author__ = "AlbertUnruh"
 __url__ = "https://github.com/AlbertUnruh/AlbertUnruhUtils.py"
 __license__ = "MIT"
 __copyright__ = f"(c) 2021 - present {__author__}"
-__version__ = "2.2.2"
+__version__ = "2.3.0"
 __description__ = "A collection of utils written in Python."
 
 from . import config
